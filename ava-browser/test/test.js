92eaf9c3b797c5eb74718e6ae7b56f9622bd59cf
deepu <deepu.c@madrasglobal.com>
2017-05-23 12:57:16 +0530

    ui testing with content test and link testing

--- ava-browser/test/test.js
+++ ava-browser/test/test.js
@@ -4,6 +4,9 @@ import {fixture} from "../index";
 
 test.beforeEach('setup fixture', fixture("./test/test.html"));
 
-test("test example", t => {
+test("h1 content", t => {
   t.is(t.context.document.documentElement.querySelector("h1").textContent, "Hello Test");
+});
+test("Checks anchor link", t => {
+  t.is(t.context.document.documentElement.querySelector(".google-link").href, "https://www.google.com/");
 });
